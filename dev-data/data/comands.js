f8b7425cf8c575c14bb036605c7968f2e6a63f9b
othmanekahtal <othmanekahtal@gmail.com>
2021-11-12 01:50:43 +0100

    add commands file in app

--- dev-data/data/comands.js
+++ dev-data/data/comands.js
@@ -2,10 +2,28 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const tourModel = require('../../models/tourModel');
 const fs = require('fs');
-const tours = () => fs.readFile('tours.json', 'utf-8');
-const deleteAll = async () => tourModel.deleteMany();
-const insertData = async () => {
-  await tourModel.insertMany(tours);
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+const deleteAll = async () => {
+  try {
+    await tourModel.deleteMany();
+    console.log('all record deleted successfully !');
+  } catch (e) {
+    console.log('error happens');
+  }
+  process.exit();
 };
-if (process.argv[2] === '--delete') deleteAll().then(() => console.log('all record deleted successfully !')).catch(() => console.log('error happens'));
-if (process.argv[2] === 'fill') insertData().then(() => console.log('all record inserted successfully !')).catch(() => console.log('error happens'));
+
+const insertData = async () => {
+  try {
+    await tourModel.insertMany(tours);
+    console.log('all record deleted successfully !');
+  } catch (e) {
+    console.log('error happens');
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--delete') deleteAll();
+if (process.argv[2] === '--fill') insertData();
