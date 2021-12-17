336c6d424a0d2a1a25453bb14db138e7375b60e3
othmane <othmanekahtal@gmail.com>
2021-12-17 21:22:28 +0100

    handling errors with duplicate fields and invalid fields

--- Controllers/globalErrorHandler.js
+++ Controllers/globalErrorHandler.js
@@ -1,4 +1,5 @@
 const ErrorHandler = require('./../utils/errorHandler');
+const { json } = require('express');
 const errorDev = (error, res) => res.status(error.statusCode).json({
   status: error.status,
   message: error.message,
@@ -20,18 +21,30 @@ const errorProd = (error, res) => {
 
 };
 module.exports = (error, req, res, next) => {
-  console.log(error);
   error.statusCode ||= 500;
   error.status ||= 'error';
   if (process.env.NODE_ENV === 'development') {
     errorDev(error, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let err = { ...error };
-    if (error.name === 'CastError') {
+    // console.log(`this is :${this.name}`);
+    let err = { ...error, name: error.name, message: error.message };
+    // console.log(Object.getPrototypeOf(error));
+    // console.log(JSON.stringify(error.__proto__));
+    if (err.name === 'CastError') {
       err = new ErrorHandler({
         message: `invalid ${err.path}:${err.value}`,
         statusCode: 400
       });
+    }
+    // to identify errors happens in duplicate unique field code = 11000
+
+    if (err.code === 11000) {
+      console.log(err.message);
+      err = new ErrorHandler({
+        message: Object.entries(err.keyValue).length === 1 ? `The field with name '${Object.entries(err.keyValue).map(el => `${el[0]} : ${el[1]}`).join(' | ')}' is duplicated ` : `The fields with names '${Object.entries(err.keyValue).map(el => `${el[0]} : ${el[1]}`).join(' | ')}' are duplicated `,
+        statusCode: 400
+      });
+
     }
     errorProd(err, res);
   }
