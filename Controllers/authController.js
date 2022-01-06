117cb920004c46ada1eb0320e52e34b04bcd26ff
othmane <othmanekahtal@gmail.com>
2022-01-06 10:53:54 +0100

    fix bug in correctPassword

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -3,24 +3,37 @@ const userModel = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const ErrorHandler = require('./../utils/errorHandler');
 const generateToken = async (id) => {
-  return await jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+  return await jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
 };
 exports.login = AsyncCatch(async (req, res, next) => {
   const { email, password } = req.body;
-  console.log();
-  if (!email || !password) return next(new ErrorHandler({ message: 'provide email and password !', statusCode: 400 }));
-  let response = await userModel.findOne({ email }).select('+password');
-
-  const passwordsMatch = await response.correctPassword({
+  if (!email || !password)
+    return next(
+      new ErrorHandler({
+        message: 'provide email and password !',
+        statusCode: 400,
+      })
+    );
+  // console.log(email);
+  let response = await userModel.findOne({ email })?.select('+password');
+  console.log(response);
+  const passwordsMatch = await response?.correctPassword({
     candidatePassword: response.password,
-    userPassword: password
+    userPassword: password,
   });
   if (response && passwordsMatch)
     return res.status(201).json({
       status: 'success',
-      token: await generateToken(response._id)
+      token: await generateToken(response._id),
     });
-  next(new ErrorHandler({ message: 'password or email is not correct', statusCode: 401 }));
+  next(
+    new ErrorHandler({
+      message: 'password or email is not correct',
+      statusCode: 401,
+    })
+  );
 });
 
 exports.signup = AsyncCatch(async (req, res) => {
@@ -32,6 +45,6 @@ exports.signup = AsyncCatch(async (req, res) => {
   res.status(201).json({
     status: 'success',
     token,
-    data: response
+    data: response,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -4,59 +4,57 @@ const { Schema } = mongoose;
 const { hashPassword, correctPassword } = require('../hooks/ModelsUtils');
 
 // we need to getting schema class in mongoose:
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: [true, 'An user must have a username'],
-      // unique: true,
-      trim: true,
-      maxlength: [40, 'An username must have less or equal then 40 characters'],
-      minlength: [10, 'An username must have more or equal then 10 characters']
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, 'An user must have a username'],
+    // unique: true,
+    trim: true,
+    maxlength: [40, 'An username must have less or equal then 40 characters'],
+    minlength: [10, 'An username must have more or equal then 10 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'An user must have an email'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'An email must have less or equal then 40 characters'],
+    minlength: [10, 'An email must have more or equal then 10 characters'],
+    // validation:validator.isEm
+    validate: {
+      message: 'An email is not valid !',
+      validator: validator.isEmail,
     },
-    email: {
-      type: String,
-      required: [true, 'An user must have an email'],
-      unique: true,
-      trim: true,
-      maxlength: [40, 'An email must have less or equal then 40 characters'],
-      minlength: [10, 'An email must have more or equal then 10 characters'],
-      // validation:validator.isEm
-      validate: {
-        message: 'An email is not valid !',
-        validator: validator.isEmail
-      }
+  },
+  password: {
+    type: String,
+    required: [true, 'An user must have an password'],
+    maxlength: [26, 'A password must have less or equal then 26 characters'],
+    minlength: [10, 'An password must have more or equal then 10 characters'],
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'please confirm your password'],
+    maxlength: [26, 'A password must have less or equal then 26 characters'],
+    minlength: [10, 'An password must have more or equal then 10 characters'],
+    validate: {
+      message: 'Passwords not the same !',
+      validator: function (element) {
+        return this.password === element;
+      },
     },
-    password: {
-      type: String,
-      required: [true, 'An user must have an password'],
-      maxlength: [26, 'A password must have less or equal then 26 characters'],
-      minlength: [10, 'An password must have more or equal then 10 characters'],
-      select: false
-    },
-    confirmPassword: {
-      type: String,
-      required: [true, 'please confirm your password'],
-      maxlength: [26, 'A password must have less or equal then 26 characters'],
-      minlength: [10, 'An password must have more or equal then 10 characters'],
-      validate: {
-        message: 'Passwords not the same !',
-        validator: function(element) {
-          return this.password === element;
-        }
-      }
-    },
-    imageCover: {
-      type: String,
-      required: [true, 'An user must have a cover image']
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      select: false
-    }
-  }
-);
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'An user must have a cover image'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+});
 userSchema.pre('save', hashPassword);
 userSchema.methods.correctPassword = correctPassword;
 // error happens when we import function in another file
