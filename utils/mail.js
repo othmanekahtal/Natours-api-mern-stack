6fd521b0c0d750b98a9bf403de416749495be5bf
othmane <othmanekahtal@gmail.com>
2022-01-27 01:57:08 +0100

    Revert "forget password done : add fonctionality of the send email"
    This reverts commit b09295ae86d7c84bd8b77b10e4efc3cb9b608832.

--- utils/mail.js
+++ utils/mail.js
@@ -8,7 +8,7 @@ const sendEmail = async (options) => {
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
-    },
+    }
   });
 
   // 2) Define the email options
@@ -23,4 +23,5 @@ const sendEmail = async (options) => {
   // 3) Actually send the email
   await transporter.sendMail(mailOptions);
 };
+
 module.exports = sendEmail;
